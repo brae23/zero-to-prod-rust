7a5b560b1bb73733353ace84e86ff07070897930
Brendan Rae <brae23hockey@gmail.com>
2023-08-19 09:46:35 -0400

    Added new canned endpoint and integration tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,28 @@
-use actix_web::{web::{self}, App, HttpRequest, HttpServer, Responder, HttpResponse};
+use actix_web::dev::Server;
+use actix_web::{web::{self}, App, HttpServer, HttpResponse};
+use std::net::TcpListener;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name: &str = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
 }
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
-pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new( || {
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new( || {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+            .route("/subscriptions", web::post().to(subscribe))
+        })
+        .listen(listener)?
+        .run();
+    Ok(server)
 }
