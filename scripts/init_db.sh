a32246be463226315f2457fb26d9cb5b1d5bf383
Brendan Rae <brae23hockey@gmail.com>
2023-08-25 08:33:52 -0400

    Restructured project, added db connection to int tests, and updated GH actions workflow yml

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -32,14 +32,17 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
 # Launch postgres using Docker
-docker run \
-    -e POSTGRES_USER=${DB_USER} \
-    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-    -e POSTGRES_DB=${DB_NAME} \
-    -p "${DB_PORT}":5432 \
-    -d postgres \
-    postgres -N 1000
-    # ^ Increases max number of connections
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+    docker run \
+        -e POSTGRES_USER=${DB_USER} \
+        -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+        -e POSTGRES_DB=${DB_NAME} \
+        -p "${DB_PORT}":5432 \
+        -d postgres \
+        postgres -N 1000
+        # ^ Increases max number of connections
+fi
 
 # Keep pinging Postgres until it's ready to accept commands
 export PGPASSWORD="${DB_PASSWORD}"
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::net::TcpListener;
-
-use zero2prod::run;
+use zero2prod::{startup::run, configuration::get_configuration};
 
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind address");
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("Failed to bind address");
     run(listener)?.await
 }
