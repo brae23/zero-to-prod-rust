5bea9dae765ea7895b0dadb6a78e41891481a7e8
Brendan Rae <brae23hockey@gmail.com>
2023-08-27 18:02:35 -0400

    Adding groundwork for telemetry

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::net::TcpListener;
 use sqlx::PgPool;
 use zero2prod::{startup::run, configuration::get_configuration};
+use env_logger::Env;
 
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("failed to read configuration.");
     let connection_pool = PgPool::connect(
--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,6 @@
 use actix_web::dev::Server;
 use actix_web::{web::{self}, App, HttpServer};
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use crate::routes::{subscribe, health_check};
@@ -11,6 +12,7 @@ pub fn run(
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
