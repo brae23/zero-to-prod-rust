4804f98b4ad6f594e925bc1d61fe8cf861ff8483
Brendan Rae <brae23hockey@gmail.com>
2023-08-31 13:50:27 -0400

    Attempt to fix init db batch file

--- scripts/init_db.bat
+++ scripts/init_db.bat
@@ -1,24 +1,6 @@
-# Check if a custom user has been set, otherwise default to 'postgres'
-DB_USER=${POSTGRES_USER:=postgres}
-
-# Check if a custom password has been set, otherwise default to 'password'
-DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
-
-# Check if a custom database name has been set, otherwise default to 'newsletter'
-DB_NAME="${POSTGRES_DB:=newsletter}"
-
-# Check if a custom port has been set, otherwise default to '5432'
-DB_PORT="${POSTGRES_PORT:=5432}"
-
-# Check if a custom host has been set, otherwise default to 'localhost'
-DB_HOST="${POSTGRES_HOST:=localhost}"
-
-# Launch postgres using Docker
-docker run \
-    -e POSTGRES_USER=${DB_USER} \
-    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-    -e POSTGRES_DB=${DB_NAME} \
-    -p "${DB_PORT}":5432 \
-    -d postgres \
-    postgres -N 1000
-    # ^ Increases max number of connections
+set "DB_USER=postgres"
+set "DB_PASSWORD=password"
+set "DB_NAME=newsletter"
+set "DB_PORT=5432"
+set "DB_HOST=localhost"
+docker run -e POSTGRES_USER=%DB_USER% -e POSTGRES_PASSWORD=%DB_PASSWORD% -e POSTGRES_DB=%DB_NAME% -p %DB_PORT% -d postgres postgres -N 1000
