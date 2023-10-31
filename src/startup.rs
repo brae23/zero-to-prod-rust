8ce5ded403cd076721b29d0ff2f1247cf6de4f0e
Brendan  Rae <brae23hockey@gmail.com>
2023-10-31 10:58:00 -0400

    Added HMAC tag to query parameters for failed login attempt redirects

--- src/startup.rs
+++ src/startup.rs
@@ -9,6 +9,7 @@ use actix_web::{
     web::{self},
     App, HttpServer,
 };
+use secrecy::Secret;
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -48,6 +49,7 @@ impl Application {
             connection_pool,
             email_client,
             configuration.application.base_url,
+            configuration.application.hmac_secret,
         )?;
 
         Ok(Self { port, server })
@@ -67,6 +69,7 @@ pub fn run(
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
+    hmac_secret: Secret<String>,
 ) -> Result<Server, std::io::Error> {
     let db_pool = Data::new(db_pool);
     let email_client = Data::new(email_client);
@@ -84,6 +87,7 @@ pub fn run(
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
+            .app_data(Data::new(HmacSecret(hmac_secret.clone())))
     })
     .listen(listener)?
     .run();
@@ -95,3 +99,6 @@ pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
         .acquire_timeout(std::time::Duration::from_secs(2))
         .connect_lazy_with(configuration.with_db())
 }
+
+#[derive(Clone)]
+pub struct HmacSecret(pub Secret<String>);
