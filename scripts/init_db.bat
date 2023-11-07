0383ccbd4d4b9b9007bfed377fd724428f70e737
Brendan  Rae <brae23hockey@gmail.com>
2023-11-07 13:24:24 -0500

    Added sessions and typed  session api for users when logged in. Added admin dashboard redirect on successful login

--- scripts/init_db.bat
+++
@@ -1,6 +0,0 @@
-set "DB_USER=postgres"
-set "DB_PASSWORD=password"
-set "DB_NAME=newsletter"
-set "DB_PORT=5432"
-set "DB_HOST=localhost"
-docker run -e POSTGRES_USER=%DB_USER% -e POSTGRES_PASSWORD=%DB_PASSWORD% -e POSTGRES_DB=%DB_NAME% -p %DB_PORT% -d postgres postgres -N 1000
