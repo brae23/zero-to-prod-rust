7ba87215f0aff883f3465b0e0731338aa2670061
Brendan  Rae <brae23hockey@gmail.com>
2023-11-09 09:25:32 -0500

    Seeded admin user and laying groundwork for password reset

---
+++ migrations/20231109134138_seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+    '50ed6a98-7bdb-4243-908d-2278fdcd1cc2',
+    'admin',
+    '$argon2id$v=19$m=15000,t=2,p=1$3bWEVHp8BABV8dBG4MHKMg$1zB4y3cONtX+wp+mT1iIOXMTRZQFpxBBjDKx+9IJnqI'
+);
